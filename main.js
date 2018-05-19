5fb4abf654b719c3ec5964f06af5e97f60d79887
Blagica Bozhinovska <blagicastojanoska@gmail.com>
2018-05-19 03:51:26 +0200

    added hover on nav links during scrolling

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const navbar = document.getElementById("navbarToggler");
+$('body').scrollspy({ target: '#navbar-example' })
 
 window.addEventListener("scroll", () => {
     return navbar.classList.remove("show");
 });
+
